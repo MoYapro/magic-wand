09ffdb51de0c2bcc9893a32833b7bf1967676472
Tom Ritmeier <tom.ritmeier@gmail.com>
2024-03-03 08:37:21 +0100

    add hint for bad drop hit rect

--- app/src/main/java/de/moyapro/colors/MagicView.kt
+++ app/src/main/java/de/moyapro/colors/MagicView.kt
@@ -1,9 +1,14 @@
 package de.moyapro.colors
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import de.moyapro.colors.wand.Magic
 import de.moyapro.colors.wand.MagicType
@@ -15,6 +20,7 @@ fun MagicView(magic: Magic = createExampleMagic(), mainViewModel: MainViewModel 
         viewModel = mainViewModel,
         dataToDrop = createExampleMagic()
     ) {
+        Box(Modifier.size(2.dp).background(Color.Black))
         Text(
             text = "❂",
             fontSize = 66.sp,
