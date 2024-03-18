6a0b82d77247c593edb17b369b0949795704e367
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2024-03-18 15:14:56 +0100

    create simple main menu

--- app/src/main/java/de/moyapro/colors/MainActivity.kt
+++ app/src/main/java/de/moyapro/colors/MainActivity.kt
@@ -3,20 +3,22 @@ package de.moyapro.colors
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import de.moyapro.colors.game.GameViewModel
 import de.moyapro.colors.ui.theme.ColorsTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val mainViewModel = MainViewModel()
-    private val gameViewModel = GameViewModel()
+    private val mainViewModel: MainViewModel by viewModels()
+    private val gameViewModel: GameViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
             ColorsTheme {
-                WandsView(gameViewModel, mainViewModel)
+                MainMenu()
+//                WandsView(gameViewModel, mainViewModel)
             }
         }
     }
---
+++ app/src/main/java/de/moyapro/colors/MainMenu.kt
@@ -0,0 +1,39 @@
+package de.moyapro.colors
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainMenu() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {}) {
+                Text("Start fight")
+            }
+            Button(onClick = {}) {
+                Text("Edit wand")
+            }
+            Button(onClick = {}) {
+                Text("Settings")
+            }
+            Button(onClick = {}) {
+                Text("End run")
+            }
+        }
+    }
+}
