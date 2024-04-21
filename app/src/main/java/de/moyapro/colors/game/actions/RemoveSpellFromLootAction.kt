9d6f4d308385aacc5ed4c550f935f373d00efdf2
Tom Ritmeier <tom.ritmeier@gmail.com>
2024-04-21 09:47:26 +0200

    add RemoveSpellFromLootAction

---
+++ app/src/main/java/de/moyapro/colors/game/actions/RemoveSpellFromLootAction.kt
@@ -0,0 +1,17 @@
+package de.moyapro.colors.game.actions
+
+import de.moyapro.colors.game.*
+import de.moyapro.colors.wand.*
+
+data class RemoveSpellFromLootAction(
+    val spell: Spell,
+) : GameAction("Remove spell from loot") {
+    override val randomSeed: Int = this.hashCode()
+
+    override fun apply(oldState: MyGameState): Result<MyGameState> {
+        require(oldState.loot.spells.contains(spell)) { "Could not remove spell from lot because it was not in there" }
+        return Result.success(
+            oldState.copy(loot = oldState.loot.copy(spells = oldState.loot.spells.filter { it.id != spell.id }))
+        )
+    }
+}
--- app/src/test/java/de/moyapro/colors/wand/actions/PlaceSpellActionTest.kt
+++ app/src/test/java/de/moyapro/colors/wand/actions/PlaceSpellActionTest.kt
@@ -6,7 +6,6 @@ import de.moyapro.colors.game.actions.*
 import de.moyapro.colors.takeTwo.*
 import de.moyapro.colors.wand.*
 import io.kotest.matchers.*
-import io.kotest.matchers.collections.*
 import org.junit.*
 
 class PlaceSpellActionTest {
@@ -28,9 +27,6 @@ class PlaceSpellActionTest {
 
         val updatedState = action.apply(state).getOrThrow()
         updatedState.findWand(wand.id)!!.slots.single { it.id == slotToPutSpellInto.id }.spell?.name shouldBe "newSpell"
-        // TODO Add onReplaceAction to placeSpellAction to move current spell into loot
-//        updatedState.loot.spells.single().name shouldBe "Blitz"
-
     }
 
     @Test
@@ -52,82 +48,6 @@ class PlaceSpellActionTest {
 
         val updatedState = action.apply(state).getOrThrow()
         updatedState.loot.findWand(wand.id)!!.slots.single { it.id == slotToPutSpellInto.id }.spell?.name shouldBe "newSpell"
-        // TODO Add onReplaceAction to placeSpellAction to move current spell into loot
-//        updatedState.loot.spells.single().name shouldBe "Blitz"
-
     }
 
-    @Test
-    fun `place spell from slot in other slot`() {
-        val wand = createExampleWand()
-        val slotToPutSpellInto = wand.slots.single { it.spell?.name == "Blitz" }
-        val slotToTakeSpellFrom = wand.slots.single { it.spell?.name == "Top" }
-        val spellToPlace = slotToTakeSpellFrom.spell!!
-        val state = MyGameState(
-            wands = listOf(wand),
-            currentTurn = 0,
-            enemies = emptyList(),
-            mages = emptyList(),
-            magicToPlay = emptyList(),
-        )
-        val action =
-            PlaceSpellAction(slotId = slotToPutSpellInto.id, spell = spellToPlace, wandId = wand.id)
-
-        val updatedState = action.apply(state).getOrThrow()
-        updatedState.findWand(wand.id)!!.slots.single { it.id == slotToPutSpellInto.id }.spell?.name shouldBe "Top"
-        updatedState.findWand(wand.id)!!.slots.single { it.id == slotToTakeSpellFrom.id }.spell?.name shouldBe "Blitz"
-        updatedState.loot.spells shouldBe emptyList()
-
-    }
-
-    @Test
-    fun `place a lot of spells`() {
-        val wand = createExampleWand()
-        val state = MyGameState(
-            wands = listOf(wand),
-            currentTurn = 0,
-            enemies = emptyList(),
-            mages = emptyList(),
-            magicToPlay = emptyList(),
-        )
-        val initialSlotIds = state.wands.single().slots.map(Slot::id)
-        var updatedState = state
-        state.wands.first().slots.forEach { slot ->
-            if (slot.spell == null) return
-            val action = PlaceSpellInLootAction(slot.spell!!)
-            updatedState = action.apply(updatedState).getOrThrow()
-
-        }
-        updatedState.loot.spells.size shouldBe initialSlotIds.size
-        updatedState.wands.single().slots.all { it.spell == null }
-        updatedState.loot.spells.forEachIndexed { index, spell ->
-            val action = PlaceSpellAction(
-                state.wands.single().id,
-                state.wands.single().slots[index].id,
-                spell
-            )
-            updatedState = action.apply(updatedState).getOrThrow()
-
-        }
-        updatedState.loot.spells shouldHaveSize 0
-        updatedState.wands.single().slots.all { it.spell != null } shouldBe true
-
-        repeat(100) { iteration ->
-            val sourceSlot = updatedState.wands.single().slots.random()
-            val targetSlot = updatedState.wands.single().slots.random()
-            check(sourceSlot.spell != null) { "Upsi: Iteration: $iteration" }
-            updatedState =
-                PlaceSpellAction(state.wands.single().id, targetSlot.id, sourceSlot.spell!!).apply(
-                    updatedState
-                ).getOrThrow()
-            val updatedSourceSlot = updatedState.findSlot(sourceSlot.id)!!
-            val updatedTargetSlot = updatedState.findSlot(targetSlot.id)!!
-            updatedSourceSlot.spell shouldBe targetSlot.spell
-            updatedTargetSlot.spell shouldBe sourceSlot.spell
-            updatedSourceSlot.spell shouldNotBe null
-            updatedTargetSlot.spell shouldNotBe null
-        }
-
-
-    }
 }
