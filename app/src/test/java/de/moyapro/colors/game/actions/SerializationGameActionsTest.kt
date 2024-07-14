f74bed5c1e9a17134005f9629ab08d5021109f1a
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2024-07-14 09:15:02 +0200

    fix + test SerializeGameAction

---
+++ app/src/test/java/de/moyapro/colors/game/actions/SerializationGameActionsTest.kt
@@ -0,0 +1,45 @@
+package de.moyapro.colors.wand
+
+import de.moyapro.colors.*
+import de.moyapro.colors.game.actions.*
+import de.moyapro.colors.game.actions.fight.*
+import de.moyapro.colors.game.model.*
+import de.moyapro.colors.util.*
+import io.kotest.matchers.equals.*
+import org.junit.*
+import org.junit.runner.*
+import org.junit.runners.*
+import org.junit.runners.Parameterized.Parameters
+
+@RunWith(Parameterized::class)
+class SerializationGameActionsTest(private val value: Any) {
+
+    companion object {
+        @Parameters(name = "{index}: {0}")
+        @JvmStatic
+        fun data(): List<GameAction> {
+            return listOf(
+                PlaceMagicAction(
+                    magicToPlace = createExampleMagic(),
+                    slotId = SlotId(),
+                    wandId = WandId()
+                ),
+                ZapAction(WandId()),
+                NoOp(),
+                EndTurnAction(),
+                TargetSelectedAction(target = EnemyId()),
+                ShowTargetSelectionAction(originalAction = ZapAction(wandId = WandId())),
+                HitAction(damage = Int.MAX_VALUE, targetMageId = MageId()),
+            )
+        }
+    }
+
+    @Test
+    fun `de-serialize single types`() {
+        val objectMapper = getConfiguredJson()
+        val internalValue: GameAction = value as GameAction
+        val json = objectMapper.writeValueAsString(internalValue)
+        val deserialized: GameAction = objectMapper.readValue(json, GameAction::class.java)
+        deserialized shouldBeEqual internalValue
+    }
+}
