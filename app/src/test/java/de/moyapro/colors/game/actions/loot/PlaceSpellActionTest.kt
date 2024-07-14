6c87d59c954a77c8d7089734cf84653aae51a795
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2024-07-14 09:48:50 +0200

    fix + test PlaceSpellActionTest

---
+++ app/src/test/java/de/moyapro/colors/game/actions/loot/PlaceSpellActionTest.kt
@@ -0,0 +1,32 @@
+package de.moyapro.colors.game.actions.loot
+
+import de.moyapro.colors.game.model.*
+import de.moyapro.colors.game.model.accessor.*
+import de.moyapro.colors.wand.*
+import io.kotest.matchers.*
+import org.junit.*
+
+class PlaceSpellActionTest {
+    @Test
+    fun `place spell in wand at hand`() {
+        val state = getExampleGameState()
+        val spellToPlace = Spell(name = "newSpell", magicSlots = emptyList())
+        val wandToPutSpellInto = state.currentRun.activeWands.first()
+        val slotToPutSpellInto = wandToPutSpellInto.slots.first()
+        val action = PlaceSpellAction(slotId = slotToPutSpellInto.id, spell = spellToPlace, wandId = wandToPutSpellInto.id)
+        val updatedState = action.apply(state).getOrThrow()
+        updatedState.currentRun.activeWands.findWand(wandToPutSpellInto.id)!!.slots.single { it.id == slotToPutSpellInto.id }.spell?.name shouldBe "newSpell"
+    }
+
+    @Test
+    fun `place spell in wand in loot`() {
+        val spellToPlace = Spell(name = "newSpell", magicSlots = emptyList())
+        val state = getExampleGameState()
+        val wandToPutSpellInto = state.currentRun.wandsInBag.first()
+        val slotToPutSpellInto = wandToPutSpellInto.slots.first()
+        val action = PlaceSpellAction(slotId = slotToPutSpellInto.id, spell = spellToPlace, wandId = wandToPutSpellInto.id)
+        val updatedState = action.apply(state).getOrThrow()
+        updatedState.currentRun.wandsInBag.findWand(wandToPutSpellInto.id)!!.slots.single { it.id == slotToPutSpellInto.id }.spell?.name shouldBe "newSpell"
+    }
+
+}
