e8b5a88635331973879f74c1ec388cd80218d26d
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2024-07-20 09:51:10 +0200

    fix + test NoOp

---
+++ app/src/test/java/de/moyapro/colors/game/actions/NoOpTest.kt
@@ -0,0 +1,14 @@
+package de.moyapro.colors.game.actions
+
+import de.moyapro.colors.wand.*
+import io.kotest.matchers.*
+import org.junit.*
+
+internal class NoOpTest {
+    @Test
+    fun `NoOp should do nothing`() {
+        val state = getExampleGameState()
+        val updatedState = NoOp().apply(state).getOrThrow()
+        state shouldBe updatedState
+    }
+}
