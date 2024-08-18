f74c987dcc631a53df190ed665fd3136c70d9e0e
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2024-08-18 09:04:19 +0200

    de-serialize world state

---
+++ app/src/test/java/de/moyapro/colors/game/actions/SerializationWorldStateTest.kt
@@ -0,0 +1,35 @@
+package de.moyapro.colors.game.actions
+
+import android.util.*
+import com.fasterxml.jackson.module.kotlin.*
+import de.moyapro.colors.game.generators.*
+import de.moyapro.colors.game.model.gameState.*
+import de.moyapro.colors.util.*
+import io.kotest.matchers.equals.*
+import io.mockk.*
+import org.junit.*
+
+class SerializationWorldStateTest {
+
+    companion object {
+        val objectMapper = getConfiguredJson()
+
+        @BeforeClass
+        @JvmStatic
+        fun setup() {
+            mockkStatic(Log::class)
+            every { Log.d(any(), any()) } returns 1
+            every { Log.e(any(), any()) } returns 1
+            every { Log.i(any(), any()) } returns 1
+        }
+
+    }
+
+    @Test
+    fun `de-serialize world state`() {
+        val gameState = StartFightFactory.setupFightStage()
+        val json = objectMapper.writeValueAsString(gameState)
+        val deserialized: NewGameState = objectMapper.readValue(json)
+        deserialized shouldBeEqual gameState
+    }
+}
