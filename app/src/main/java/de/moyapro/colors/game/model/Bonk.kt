7eeec636ec748860bceeca24ab3a4c9f9f6954f8
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2024-09-11 11:17:04 +0200

    extract spells to their own files

---
+++ app/src/main/java/de/moyapro/colors/game/model/Bonk.kt
@@ -0,0 +1,14 @@
+package de.moyapro.colors.game.model
+
+import de.moyapro.colors.*
+import de.moyapro.colors.game.effect.*
+
+class Bonk(
+    id: SpellId = SpellId(),
+    magicSlots: List<MagicSlot> = listOf(createExampleMagicSlot(readyToZap = true)),
+    effects: List<Effect> = emptyList(),
+) : Spell<Bonk>(id = id, name = "Bonk", magicSlots = magicSlots, effects = effects) {
+    override fun copy(magicSlots: List<MagicSlot>): Bonk {
+        return Bonk(this.id, magicSlots, this.effects)
+    }
+}
--- app/src/main/java/de/moyapro/colors/game/model/Spell.kt
+++ app/src/main/java/de/moyapro/colors/game/model/Spell.kt
@@ -1,10 +1,7 @@
 package de.moyapro.colors.game.model
 
 import com.fasterxml.jackson.annotation.*
-import de.moyapro.colors.*
 import de.moyapro.colors.game.effect.*
-import de.moyapro.colors.game.effect.Effect.ELECTRIFIED
-import de.moyapro.colors.game.effect.Effect.WET
 import de.moyapro.colors.game.enemy.*
 import de.moyapro.colors.util.*
 
@@ -69,45 +66,3 @@ abstract class Spell<SPELL_TYPE : Spell<SPELL_TYPE>>(
         return result
     }
 }
-
-class Bonk(
-    id: SpellId = SpellId(),
-    magicSlots: List<MagicSlot> = listOf(createExampleMagicSlot(readyToZap = true)),
-    effects: List<Effect> = emptyList(),
-) : Spell<Bonk>(id = id, name = "Bonk", magicSlots = magicSlots, effects = effects) {
-    override fun copy(magicSlots: List<MagicSlot>): Bonk {
-        return Bonk(this.id, magicSlots, this.effects)
-    }
-}
-
-class Splash(
-    id: SpellId = SpellId(),
-    magicSlots: List<MagicSlot> = listOf(createExampleMagicSlot(readyToZap = true)),
-    effects: List<Effect> = listOf(WET),
-) : Spell<Splash>(id = id, name = "Splash", magicSlots = magicSlots, effects = effects) {
-    override fun copy(magicSlots: List<MagicSlot>): Splash {
-        return Splash(this.id, magicSlots, this.effects)
-    }
-}
-
-class Fizz(
-    id: SpellId = SpellId(),
-    magicSlots: List<MagicSlot> = listOf(createExampleMagicSlot(readyToZap = true)),
-    effects: List<Effect> = listOf(ELECTRIFIED),
-) : Spell<Fizz>(id = id, name = "Fizz", magicSlots = magicSlots, effects = effects) {
-    override fun copy(magicSlots: List<MagicSlot>): Fizz {
-        return Fizz(this.id, magicSlots, this.effects)
-    }
-
-    override fun applyEffect(enemy: Enemy, power: Int): Enemy {
-        val allEff = super.applyEffect(enemy, power)
-        val currentEff = allEff.statusEffects.mapNotNull { (effect, amount) ->
-            if (effect == WET) {
-                if (amount <= 1) null // to remove it
-                else WET to amount - 1
-            } else effect to amount
-        }
-        return allEff.copy(statusEffects = currentEff.associate { it })
-    }
-}
-
