6e97dfd6fd42e3235547b8caefb44b558063b7e1
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2024-11-17 11:29:13 +0100

    update version string

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "de.moyapro.colors"
         minSdk = 34
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
