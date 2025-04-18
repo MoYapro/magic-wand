7b5b04aa4fb94b25005bee25f3c9915be11cc8d2
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2025-04-18 09:17:46 +0200

    improve magicSlotPreview

--- app/src/main/java/de/moyapro/colors/ui/view/components/MagicSlotView.kt
+++ app/src/main/java/de/moyapro/colors/ui/view/components/MagicSlotView.kt
@@ -1,6 +1,7 @@
 package de.moyapro.colors.ui.view.components
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,21 +10,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.moyapro.colors.createExampleMagicSlot
 import de.moyapro.colors.game.model.MagicSlot
+import de.moyapro.colors.game.model.MagicType
 import de.moyapro.colors.game.model.hasRequiredMagic
 import de.moyapro.colors.util.DROP_ZONE_ALPHA
 import de.moyapro.colors.util.MAGIC_SIZE
 
 @Composable
-@Preview
 fun MagicSlotView(
     modifier: Modifier = Modifier,
-    magicSlot: MagicSlot = createExampleMagicSlot(),
+    magicSlot: MagicSlot,
 ) {
     val requiredMagic = magicSlot.requiredMagic
     val magicColor = requiredMagic.type.color.copy(alpha = if (magicSlot.hasRequiredMagic()) 1f else DROP_ZONE_ALPHA)
-    Canvas(modifier = modifier
-        .size(MAGIC_SIZE.dp)
-        .testTag("${requiredMagic.id}_Magic"), onDraw = {
-        drawCircle(color = magicColor)
-    })
+    Canvas(
+        modifier = modifier
+            .size(MAGIC_SIZE.dp)
+            .testTag("${requiredMagic.id}_Magic"), onDraw = {
+            drawCircle(color = magicColor)
+        })
 }
+
+
+@Composable
+@Preview
+fun MagicSlotViewPreview() {
+    Column {
+        MagicType.values().forEach {
+            MagicSlotView(magicSlot = createExampleMagicSlot(it))
+        }
+    }
+}
+
