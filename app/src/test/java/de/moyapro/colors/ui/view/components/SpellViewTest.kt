ea1338743fc2df16aef66d0a2076322e95b3d031
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2025-04-21 09:28:10 +0200

    add spellview click test

--- app/src/test/java/de/moyapro/colors/ui/view/components/SpellViewTest.kt
+++ app/src/test/java/de/moyapro/colors/ui/view/components/SpellViewTest.kt
@@ -2,9 +2,12 @@ package de.moyapro.colors.ui.view.components
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import de.moyapro.colors.game.model.Spell
 import de.moyapro.colors.game.spell.Fizz
+import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.nulls.shouldNotBeNull
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,12 +19,26 @@ class SpellViewTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun spellViewTest() {
+    fun `spellView renders`() {
         val spell: Spell<Fizz> = Fizz()
         composeTestRule.setContent {
             SpellView(spell)
         }
         composeTestRule.onNodeWithTag("${spell.id.id}_${spell.name}").assertExists()
+    }
+
+    @Test
+    fun `spellView clicks`() {
+        var clickedElement: Spell<*>? = null
+        val click = { clickedSpell: Spell<*> -> clickedElement = clickedSpell }
+        val spell: Spell<Fizz> = Fizz()
+        composeTestRule.setContent {
+            SpellView(spell = spell, clickAction = click)
+        }
+        composeTestRule.onNodeWithTag("${spell.id.id}_${spell.name}").performClick()
+        clickedElement shouldNotBeNull {
+            this shouldBeEqual spell
+        }
     }
 }
 
